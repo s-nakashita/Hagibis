inverse rotation program made / the cause of interpolation bug found; data not cyclic

rotate/rotate_scalar_glb.py
@@ -33,7 +33,7 @@ var_sfc = ['msl','t2m','d2m']
 var_pl = ['t','q','gh']
 
 lonin,latin = librotate.generate_points(nlon,nlat,dlat)
-logging.info(f"input coordinate lon:{lonin} lat:{latin}")
+logging.info(f"input coordinate lon:{lonin} \n lat:{latin}")
 #print(lonin)
 #print(latin)
 
@@ -56,7 +56,8 @@ with trackf.open() as track:
     
         lonout,latout = librotate.rotate_lonlat(lonc,latc,lonin,latin)
         logging.debug(\
-            f"output coordinate lon:max{lonout.max():.3f}, min{lonout.min():.3f} \n lat:max{latout.max():.3f},min{latout.min():.3f}")
+            f"output coordinate lon:max{lonout.max():.3f}, min{lonout.min():.3f} \n \
+                 lat:max{latout.max():.3f},min{latout.min():.3f}")
     
         data = xr.open_dataset(datadir / innc).sel(time=date)
         logging.debug(data)
@@ -81,13 +82,19 @@ with trackf.open() as track:
             inattrs = datain.attrs
             #print(inattrs)
             data_interp = datain.interp(longitude=newlon, latitude=newlat)
-            #print(data_interp)
+            logging.debug(data_interp)
             ##missing value
-            df = data_interp.to_pandas()
-            if(df.isnull().values.sum() != 0):
-                logging.warning("missing value exists in interpolation data.")
-                continue
+            #df = data_interp.to_pandas()
             value = data_interp.values
+            if(np.isnan(value).sum() != 0):
+                logging.warning("#{} missing value exists in interpolation data.".format\
+                (np.isnan(value).sum()))
+                msk = np.isnan(value)
+                mislon = newlon[msk]
+                mislat = newlat[msk]
+                for i in range(len(mislon)):
+                    logging.warning("missing at lon{} lat{}".format(mislon[i].values,mislat[i].values))
+                continue
             #print(value)
             dataout = xr.DataArray(value.reshape(1,nlat,nlon),\
                                    [('time',pd.date_range(date,periods=1)),\
@@ -111,11 +118,11 @@ with trackf.open() as track:
             data_interp = \
                 datain.interp(longitude=newlon, latitude=newlat)
             ##missing value
-            df = data_interp.to_pandas()
-            if(df.isnull().values.sum() != 0):
+            #df = data_interp.to_pandas()
+            value = data_interp.values
+            if(np.isnan(value).sum() != 0):
                 logging.warning("missing value exists in interpolation data.")
                 continue
-            value = data_interp.values
             dataout = xr.DataArray(value.reshape(1,nlev,nlat,nlon),\
                                    [('time',pd.date_range(date,periods=1)),\
                                     ('level',lev),\

rotate/rotate_uv_glb.py
@@ -33,7 +33,7 @@ var_sfc = ['u10','v10']
 var_pl  = ['u','v']
 
 lonin,latin = librotate.generate_points(nlon,nlat,dlat)
-logging.info(f"input coordinate lon:{lonin} lat:{latin}")
+logging.info(f"input coordinate lon:{lonin} \n lat:{latin}")
 
 da_np = []
 with trackf.open() as track:
@@ -100,12 +100,17 @@ with trackf.open() as track:
         yd_interp = da_yd.interp(longitude=newlon, latitude=newlat)
         zd_interp = da_zd.interp(longitude=newlon, latitude=newlat)
         #missing value
-        dfx = xd_interp.to_pandas()
-        dfy = yd_interp.to_pandas()
-        dfz = zd_interp.to_pandas()
-        if(dfx.isnull().values.sum() != 0 or dfy.isnull().values.sum() != 0\
-            or dfz.isnull().values.sum() != 0):
+        dfx = xd_interp.values
+        dfy = yd_interp.values
+        dfz = zd_interp.values
+        if(np.isnan(dfx).sum() != 0 or np.isnan(dfy).sum() != 0\
+            or np.isnan(dfz).sum() != 0):
             logging.warning("missing value exists in interpolation")
+            msk = np.isnan(dfx[0,:])
+            mislon = newlon.values[msk]
+            mislat = newlat.values[msk]
+            for i in range(len(mislon)):
+                logging.warning("missing at lon{} lat{}".format(mislon[i],mislat[i]))
             continue
         xdtc = xd_interp.values.reshape(nlat,nlon)
         ydtc = yd_interp.values.reshape(nlat,nlon)
@@ -205,13 +210,17 @@ with trackf.open() as track:
         #    dfx = xd_interp.to_pandas()
         #    dfy = yd_interp.to_pandas()
         #    dfz = zd_interp.to_pandas()
-        dfx = np.isnan(xd_interp).astype(np.int)
-        dfy = np.isnan(yd_interp).astype(np.int)
-        dfz = np.isnan(zd_interp).astype(np.int)
-        #    if(dfx.isnull().values.sum() != 0 or dfy.isnull().values.sum() != 0\
-        #        or dfz.isnull().values.sum() != 0):
-        if(np.sum(dfx) != 0 or np.sum(dfy) != 0 or np.sum(dfz) != 0):
+        dfx = xd_interp.values
+        dfy = yd_interp.values
+        dfz = zd_interp.values
+        if(np.isnan(dfx).sum() != 0 or np.isnan(dfy).sum() != 0\
+            or np.isnan(dfz).sum() != 0):
             logging.warning("missing value exists in interpolation")
+            msk = np.isnan(dfx[0,:])
+            mislon = newlon.values[msk]
+            mislat = newlat.values[msk]
+            for i in range(len(mislon)):
+                logging.warning("missing at lon{} lat{}".format(mislon[i],mislat[i]))
             continue
         xdtc = xd_interp.values.reshape(1,nlev,nlat,nlon)
         ydtc = yd_interp.values.reshape(1,nlev,nlat,nlon)
@@ -232,6 +241,11 @@ with trackf.open() as track:
         latnp = latin[None, None, :, None]
             #unp,vnp = librotate.xyzd2uv(xdnp,ydnp,zdnp,lonnp)
         unp,vnp = librotate.xyzd2uv(xdnp,ydnp,zdnp,lonnp,latnp)
+        #debug
+        xb, yb, zb = librotate.uv2xyzd(unp,vnp,lonnp,latnp)
+        logging.debug(f"xyzd2uv x {np.max(np.abs(xdnp-xb))}")
+        logging.debug(f"xyzd2uv y {np.max(np.abs(ydnp-yb))}")
+        logging.debug(f"xyzd2uv z {np.max(np.abs(zdnp-zb))}")
         #missing value
         if(np.isnan(unp).sum() != 0 or np.isnan(vnp).sum() != 0):
             logging.warning("missing value exists in xyzd2uv")